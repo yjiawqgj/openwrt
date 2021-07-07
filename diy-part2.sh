08a27796ce54feedabd726a29463464fec5a31b1
yjiawqgj <71002956+yjiawqgj@users.noreply.github.com>
2021-07-07 19:33:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,18 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.15/g' package/base-files/files/bin/config_generate
+rm -rf ./package/lean/default-settings/files/zzz-default-settings
+curl -fsSL  https://raw.githubusercontent.com/yjiawqgj/config/main/zzz-default-settings > ./package/lean/default-settings/files/zzz-default-settings
+rm -rf ./package/lean/autocore/files/x86/autocore
+curl -fsSL  https://raw.githubusercontent.com/yjiawqgj/config/main/autocore > ./package/lean/autocore/files/x86/autocore
+rm -rf ./package/lean/autocore/files/x86/index.htm
+curl -fsSL https://raw.githubusercontent.com/yjiawqgj/config/main/x86.htm > package/lean/autocore/files/x86/index.htm
+rm -rf ./package/lean/autocore/files/arm/index.htm
+curl -fsSL https://raw.githubusercontent.com/yjiawqgj/config/main/arm.htm > package/lean/autocore/files/arm/index.htm
+rm -rf ./package/lean/netdata
+git clone https://github.com/yjiawqgj/luci-app-netmap package/luci-app-netmap
+git clone https://github.com/sirpdboy/sirpdboy-package package/diy
+# git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
+sed -i 's/解锁网易云灰色歌曲/解锁灰色歌曲/g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
+git clone -b master --single-branch https://github.com/yjiawqgj/OpenAppFilter ./package/diy/OpenAppFilter
